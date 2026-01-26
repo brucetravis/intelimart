27140d64dd767d2afa74907d44e68b63da862a02
Bruce Ambundo <bruceambundo@gmail.com>
2026-01-26 14:24:41 +0300

    Products page

---
+++ src/pages/products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Products.css'
+
+export default function Products() {
+  return (
+    <section
+        className='products-page'
+    >
+
+    </section>
+  )
+}
