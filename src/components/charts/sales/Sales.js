f16f86e614470832ac70dca355e2ed0b020d8f1e
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-13 11:51:41 +0300

    Analytics page desigend accordingly

--- src/components/charts/sales/Sales.js
+++ src/components/charts/sales/Sales.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import './Sales.css'
-import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 export default function Sales({ data }) {
   return (
-    <section
+    <main
         className='monthly-sales-chart'
     >
         <h4>Monthly Sales</h4>
 
         <ResponsiveContainer width="100%" height={320} style={{ outlie: "none" }}>
-            <BarChart data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-                <XAxis dataKey={data.month} label={{ value: 'Month', position: 'insideBottom', offset: -5 }} />
+            <LineChart data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+                <XAxis dataKey="month" label={{ value: 'Month', position: 'insideBottom', offset: -5 }} />
                 <YAxis label={{ value: 'Sales', angle: -90, position: 'insideLeft', offset: 10 }} />
                 <Tooltip />
-                <Bar dataKey="sales" fill="#3b82f6" />
-            </BarChart>
+                <Line
+                    type='monotone'
+                    dataKey="sales" 
+                    stroke="#3b82f6"
+                    strokewidth={3}
+                />
+            </LineChart>
         </ResponsiveContainer>
         
-    </section>
+    </main>
   )
 }
