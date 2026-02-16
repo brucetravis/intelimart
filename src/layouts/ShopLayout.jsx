1811350e420b4c47cea518ef29daf519ba332639
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-16 13:20:24 +0300

    Admin and Shop Layout created

---
+++ src/layouts/ShopLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ShopLayout({ children }) {
+  return (
+    <div>
+        
+        <div style={{ padding: '20px' }}>
+            {children}
+        </div>
+    </div>
+  )
+}
