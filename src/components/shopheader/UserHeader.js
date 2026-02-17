0c44d616251168eaccdf2633ec7cb545b3cc3f79
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-17 09:37:45 +0300

    Cart created

--- src/components/shopheader/UserHeader.js
+++ src/components/shopheader/UserHeader.js
@@ -1,42 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UserHeader.css'
 import { Heart, Search, ShoppingBag, User2Icon } from 'lucide-react'
+import Cart from '../cart/Cart'
+import { AnimatePresence } from 'framer-motion'
 
 export default function UserHeader() {
+    // state to open and close teh cart
+    const [ openCart, setOpenCart ] = useState(false) // initially the cart is closed
+
+    // function to close the cart
+    const closeCart = (prev) => {
+        setOpenCart(!prev)
+    }
+
   return (
-    <header className='shopHeader'>
-        <div className='logo'>
-            <h4>INTELIMART</h4>
-        </div>
+    <>
+        <header className='shopHeader'>
+            <div className='logo'>
+                <h4>INTELIMART</h4>
+            </div>
 
-        {/* <div>
-            <input
-                type='text'
-                placeholder='Search electronics, clothes etc'   
-            />
-        </div> */}
-        
-        <div className='icons'>
-            <User2Icon 
-                size={28} 
-                stroke='#334155' 
-            />
-
-            <Search 
-                size={28} 
-                stroke='#334155'
-            />
-
-            <Heart
-                size={28} 
-                stroke='#334155'
-            />
+            {/* <div>
+                <input
+                    type='text'
+                    placeholder='Search electronics, clothes etc'   
+                />
+            </div> */}
             
-            <ShoppingBag 
-                size={28} 
-                stroke='#334155' 
-            />
-        </div>
-    </header>
+            <div className='icons'>
+                <User2Icon 
+                    size={28} 
+                    stroke='#334155' 
+                />
+
+                <Search 
+                    size={28} 
+                    stroke='#334155'
+                />
+
+                <Heart
+                    size={28} 
+                    stroke='#334155'
+                />
+                
+                <ShoppingBag 
+                    size={28} 
+                    stroke='#334155'
+                    onClick={() => setOpenCart(prev => !prev)}
+                />
+            </div>
+        </header>
+        
+        <AnimatePresence>
+            {openCart && <Cart onClose={closeCart} />}
+        </AnimatePresence>
+    </>
   )
 }
