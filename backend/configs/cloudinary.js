13dae330153a6e5e9421afec1560ba38a7206d1e
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-23 11:03:12 +0300

    App connected to remote cloudinary. Product modal developed

---
+++ backend/configs/cloudinary.js
@@ -0,0 +1,12 @@
+// import the cloudinary modern v2 version
+const cloudinary = require('cloudinary').v2
+
+// configure the cloudinary v2 version with the correct configurations in order to connect to your account
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
+
+//  export the configured cloudinary package
+module.exports = cloudinary
--- src/components/modals/product/Product.js
+++ src/components/modals/product/Product.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Product.css'
-import { XCircle } from 'lucide-react'
+import { ArrowUp, XCircle } from 'lucide-react'
 
 export default function Product({ onClose }) {
   return (
@@ -9,17 +9,87 @@ export default function Product({ onClose }) {
         className='product-form'
       >
         <XCircle 
-          size={20}
+          size={24}
           stroke="#ff2b14"
           className='close-btn'
           onClick={onClose}
         />
 
-        <div>
-          <div></div>
+        <h4 className='text-center mt-3 mb-4'>UPLOAD PRODUCT</h4>
+
+        <div className='upload-product-div'>
+          <div className='product-input'>
+            <h6>Category:</h6>
+            <input 
+              type='text'
+              placeholder='Hoodies'
+              required
+            />
+          </div>
           
-          <div></div>
+          <div className='product-input'>
+            <h6>Name: </h6>
+            <input 
+              type='text'
+              placeholder='African Hoodie...'
+              required
+            />
+          </div>
         </div>
+
+        <div className='upload-product-div'>
+          <div className='product-input'>
+            <h6>Quantity:</h6>
+            <input 
+              type='number'
+              placeholder='5'
+              required
+            />
+          </div>
+          
+          <div className='product-input'>
+            <h6>Price: </h6>
+            <input 
+              type='number'
+              step="0.01" // allows decial values like 2 decimal places
+              min="0" // prevents negative prices
+              placeholder={`$23.99`}
+              required
+            />
+          </div>
+        </div>
+
+        <div className='upload-product-div'>
+          <div className='product-input'>
+            <h6>Total: </h6>
+            <input 
+              type='number'
+              placeholder='$100'
+              disabled
+            />
+          </div>
+        </div>
+
+        <div className='upload-product-div'>
+          <div className='product-input'>
+            <h6>Description: </h6>
+            
+            <textarea
+              placeholder='Enter Product Description'
+              rows="4"
+              required
+            >
+
+            </textarea>
+          </div>
+        </div>
+
+
+        <button type='submit'>
+          <ArrowUp size={20} color='#fff'/>
+          Upload Product
+        </button>
+
       </form>
     </div>
   )
