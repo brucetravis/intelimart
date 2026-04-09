6773cf64cd3f10601e8c35794f72b86dd2944987
Bruce Ambundo <bruceambundo@gmail.com>
2026-04-09 17:00:14 +0300

    User layout created

---
+++ src/layouts/ShopLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import UserHeader from '../components/shopheader/UserHeader'
+import { useLocation } from 'react-router'
+
+export default function ShopLayout({ children }) {
+
+  // useLocation to keep track of where a user is
+  const location = useLocation()
+
+  return (
+    <div style={{ flex: 1 }}>
+      {location.pathname === '/users/checkout' ? (
+        ''
+      ) : (
+        <UserHeader />
+      )}
+
+      <div style={{ padding: '20px' }}>
+        {children}
+      </div>
+    </div>
+  )
+}
