d5e1311ef26beaa3e660b54ef1c496ca73f20092
Bruce Ambundo <bruceambundo@gmail.com>
2026-04-14 18:00:31 +0300

    array commented out

--- src/components/adverts/sale/Sale.js
+++ src/components/adverts/sale/Sale.js
@@ -1,38 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Sale.css'
 import { useNavigate } from 'react-router-dom'
 
 export default function Sales() {
-
-    // // salesData.js
-    // const salesItems = [
-    //     {
-    //         id: 1,
-    //         title: "Men's Fashion",
-    //         subtitle: "Season Sale",
-    //         discount: "Min. 35-70% Off",
-    //         image: require('../../../images/modelnoback.png'),
-    //         link: "/users/shop"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Women's Fashion",
-    //         subtitle: "Exclusive Deals",
-    //         discount: "Up to 50% Off",
-    //         image: require('../../../images/women-model.jpg'),
-    //         link: "/users/shop"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "Accessories",
-    //         subtitle: "Hot Picks",
-    //         discount: "Flat 40% Off",
-    //         image: require('../../../images/modelnoback.png'),
-    //         link: "/users/shop"
-    //     }
-    // ]
-    // // state to keep track of of the image in the cycle
-    // const [ currentSlide, setCurentSlide ] = useState(0) // current state holds the first slide
 
     // function to navigate to another page
     const navigate = useNavigate()
